4e29e8f82d81958215ba1a0660e4e98a880c8718
Manuel A. Márquez <buzondemam@gmail.com>
2015-12-13 11:45:34 -0430

    Functional test now checks we can input a to-do item

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse(b'<html><title>To-Do Lists</title></html>')
